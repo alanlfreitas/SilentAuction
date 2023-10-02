bdf59296ce7243727d53e5e4cccdd6cfa4c1362e
alanlfreitas <113162737+alanlfreitas@users.noreply.github.com>
2023-10-02 16:53:42 +0000

    Models of lists and dictionaries created + a grading program added

--- main.py
+++ main.py
@@ -1,1 +1,88 @@
 # -------------------- Silent Auction -------------------- #
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.",
+  "Function": "A piece of code that you can easily call over and over again.",
+}
+# #Retrieving item from dictionary
+# # print(programming_dictionary["Bug"])
+
+# #Adding new items to dictionary
+# programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+
+# #Creating an empty dictionary
+# empty_dictionary = {}
+
+# #wipe and existing dictionary
+# #programming_dictionary = {}
+# # print(programming_dictionary)
+
+# #Editing and item in the dictionary
+# programming_dictionary["Bug"] = "Just a bug."
+# # print(programming_dictionary)
+
+# #Loop through a dictionary
+# for key in programming_dictionary:
+#   print(key)
+#   print(programming_dictionary[key])
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+student_grades = {}
+
+for student in student_scores:
+  score = student_scores[student]
+  if score > 90:
+    student_grades[student] = "Outstanding"
+  elif score > 80:
+    student_grades[student] = "Exceeds Expectations"
+  elif score > 70:
+    student_grades[student] = "Acceptable"
+  else:
+    student_grades[student] = "Fail"
+
+print(student_grades)
+
+########## Nesting ##########
+capitals = {
+  "France": "Paris",
+  "Germany": "Berlin",
+}
+
+# Nesting a List in a Dictionary
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"],
+  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+# Nesting a Dictionary in a Dictionary
+travel_log = {
+  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 3},
+  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 3},
+  "Brazil": {"cities_visited": ["Fortaleza", "Rio", "Presidente Figueiredo", "Itacoatiara", "Iranduba", "Boa Vista"], "total_visits": 7}
+}
+
+# Nesting Dictionary in a List
+
+travel_log = [
+  {
+    "country": "France", 
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 3
+  },
+  {
+    "country": "Germany",
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 3
+  },
+  {
+    "country": "Brazil",
+    "cities_visited": ["Fortaleza", "Rio", "Presidente Figueiredo", "Itacoatiara", "Iranduba", "Boa Vista"],
+    "total_visits": 7
+  }
+]
